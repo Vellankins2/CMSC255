976f68fae61986801a04cf2f43bd454d378e48c1
Vellankins2 <69913376+Vellankins2@users.noreply.github.com>
2020-08-19 11:46:00 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,17 @@
+/*********************************
+* Lab 1 
+**********************************
+* Niketh Vellanki
+* 8/19/2020
+* CMSC 255 Section C90
+**********************************/
+
+public class Hello{
+	public static void main(String[] args){
+	System.out.println("Niketh Vellanki");
+	System.out.println("Lab 1");
+	System.out.println("08/19");
+	System.out.println("CMSC 255 Section C90");
+	System.out.println();
+	}
+}
